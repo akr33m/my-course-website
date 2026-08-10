ad575201bcf78aec5c674402f62d097cd1bc261d
akr33m <92796485+akr33m@users.noreply.github.com>
2026-08-11 02:43:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    const id = link.getAttribute('href');
+    if (id.length > 1) {
+      const target = document.querySelector(id);
+      if (target) {
+        e.preventDefault();
+        target.scrollIntoView({behavior:'smooth', block:'start'});
+      }
+    }
+  });
+});
